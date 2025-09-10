62e9baa9b5b85ffd0d6bc3a8c4790598af47843c
Ivan Abramov <work-abramov@mail.ru>
2025-09-10 18:22:08 +0300

    Add RPGSystem project to LearningProjects

---
+++ LearningProjects/RPGSystem/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPGSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item knife = new Item("Knife", 3, 10, ItemType.Weapon);
+            Inventory inventory = new Inventory();
+            inventory.AddItem(knife);
+            inventory.ShowItems();
+        }
+    }
+
+    public enum ItemType
+    {
+        Weapon,
+        Potion,
+        Equipment,
+        Other
+    }
+    public class Item
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Value { get; set; }
+        public ItemType Type { get; set; }
+        public Item(string name, double weight, int value, ItemType type)
+        {
+            Name = name;
+            Weight = weight;
+            Value = value;
+            Type = type;
+        }
+    }
+
+    public class Inventory
+    {
+        public List<Item> Items { get; set; }
+        public double TotalWeight { get; private set; }
+        public Inventory()
+        {
+            Items = new List<Item>();
+        }
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+            Console.WriteLine($"Item {item.Name} has been added to inventory");
+        }
+        public void RemoveItem(Item item) 
+        { 
+            Items.Remove(item);
+            Console.WriteLine($"Item {item.Name} has been removed from inventory");
+        }
+        public void ShowItems()
+        {
+            int i = 1;
+            Console.WriteLine("List of items:");
+            foreach (Item item in Items)
+            {
+                Console.WriteLine($"{i}. Name - {item.Name};   Weight - {item.Weight}; " +
+                    $"  Value - {item.Value};   Item type - {item.Type}");
+            }
+        }
+    }
+
+    public class Player 
+    { 
+        //in progress
+    }
+
+}                    
---
+++ LearningProjects/RPGSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RPGSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RPGSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("173bb909-cbba-4553-8ae7-65b7b4ded162")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
