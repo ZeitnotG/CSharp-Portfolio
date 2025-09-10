c53cc603478ff68f64b3b835bc88f8b1bd42a402
Ivan Abramov <work-abramov@mail.ru>
2025-09-10 16:48:48 +0300

    Add Calculator mini-project to LearningProjects

---
+++ LearningProjects/Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите желаемую операцию (*,/, +, -): ");
+        string op = Console.ReadLine();
+        switch (op)
+        {
+            case "*":
+                Console.WriteLine($"Произведение чисел: {number1 * number2}");
+                break;
+            case "/":
+                Console.WriteLine($"Частное чисел: {(double)number1 / number2}");
+                break;
+            case "+":
+                Console.WriteLine($"Сумма чисел: {number1 + number2}");
+                break;
+            case "-":
+                Console.WriteLine($"Разница чисел: {number1 - number2}");
+                break;
+            default:
+                Console.WriteLine("Выбрана неверная операция");
+                break;
+        }
+        
+        
+    }
+}
+
+
