2960540d8ae5323387af269d69674ec151a94cd0
Ivan Abramov <work-abramov@mail.ru>
2025-09-13 23:06:42 +0300

    feat: add parent class Animal to Farm project

---
+++ LearningProjects/Farm/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Farm
+{
+    public enum ProductType
+    {
+        Milk,
+        Egg,
+        Meal
+    }
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Health {  get; set; }
+        public int Hunger {  get; set; }
+        public ProductType productType { get; set; }
+        public int Produce(ProductType productType)
+        {
+            int amount = 0;
+            return amount;
+        }
+    }
+}
---
+++ LearningProjects/Farm/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Farm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
