921ced312bda3f8599e6251c989cf6362cc4389e
Ivan Abramov <work-abramov@mail.ru>
2025-09-16 12:21:12 +0300

    feat: Add game cycle

--- LearningProjects/Farm/Core/Game.cs
+++ LearningProjects/Farm/Core/Game.cs
@@ -4,23 +4,51 @@ namespace FarmSim
 {
     internal class Game
     {
+        private int day;
+        private FarmSim.Core.Farm farm;
         static void Main(string[] args)
         {
-            FarmSim.Core.Farm farm = new FarmSim.Core.Farm();
-
-            farm.AddToStorage(ProductType.Hay, 10);
-            farm.AddToStorage(ProductType.Corn, 10);
-
             Animal chicken = new Chicken("Chick", 30, 40);
             Animal cow = new Cow("Cow", 30, 40);
-            farm.AddAnimal(chicken);
-            farm.AddAnimal(cow);
-            farm.FeedAnimals();
-            chicken.ShowInfo();
-            cow.ShowInfo();
-            farm.CollectProducts();
-            farm.Sell();
-            farm.Buy(ProductType.Hay, 5);
         }
+      
+        public Game()
+        {
+            day = 1;
+            farm = new FarmSim.Core.Farm();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine($"\n=== Day {day} ===");
+
+                farm.FeedAnimals();
+
+                farm.CollectProducts();
+
+                Console.WriteLine("Do you want to sell or buy? sell/buy/skip");
+                string action = Console.ReadLine();
+                if (action == "sell")
+                    farm.Sell();
+                else if (action == "buy")
+                {
+                    Console.WriteLine("What do you want to buy? Hay/Corn");
+                    string typeInput = Console.ReadLine();
+                    if (Enum.TryParse<ProductType>(typeInput, out ProductType productType))
+                    {
+                        Console.WriteLine("How many?");
+                        int quantity = int.Parse(Console.ReadLine());
+                        farm.Buy(productType, quantity);
+                    }
+                }
+                else 
+                    Console.WriteLine("Unknown command");
+
+               day++;
+            }
+        }
+
     }
 }
